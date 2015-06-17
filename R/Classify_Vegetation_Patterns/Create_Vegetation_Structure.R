c9dd00888d96366716bab0e9fc3340ffac3ed971
BasBoek <bastiaen.boekelo@wur.nl>
2015-06-17 12:24:38 +0200

    added training areas data

--- R/Classify_Vegetation_Patterns/Create_Vegetation_Structure.R
+++ R/Classify_Vegetation_Patterns/Create_Vegetation_Structure.R
@@ -2,4 +2,10 @@
 # Date: June 17, 2015
 # Goal: Supervised Classification of the height rasters to obtain vegetation classes
 
+rm(list=ls())
 
+
+library(raster)
+
+Training_areas <- shapefile("data/Training_Areas/Training_Areas_AfterRevision.shp")
+str(Training_areas)
