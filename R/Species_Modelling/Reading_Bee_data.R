be5b702d13d9a380f6303e901af91557e915ca77
BasBoek <bastiaen.boekelo@wur.nl>
2015-07-09 14:01:20 +0200

    Finished script and made the right input / output table for the bees

--- R/Species_Modelling/Reading_Bee_data.R
+++ R/Species_Modelling/Reading_Bee_data.R
@@ -5,32 +5,29 @@
 
 rm(list=ls())
 
+Source_loc <- "data/Bee_data/FOR_R_origin_from_Bee_data_Prepared_for_R.csv"
 
-Source_loc <- "C:/Users/BranderPC/Dropbox/WUR/Thesis GIS/Data/BeeData_Adjusted/South_from_2000_WithoutDoubles_for_R.csv"
+# Subsetting South of the Netherlands and relevant columns
 
 Bees_all <- read.csv(Source_loc, header=T, sep=";")
-Bees_all <- Bees_all[,1:9]
-Bees_from2002 <- subset(Bees_all, JAAR >2001)
-Bees_from2003 <- subset(Bees_all, JAAR >2002)
-Bees_from2004 <- subset(Bees_all, JAAR >2003)
-Bees_from2005 <- subset(Bees_all, JAAR >2004)
-
+Bees_all <- subset(Bees_all, Y_Round < 475)
+Bees_all <- Bees_all[,c(1,2,3,4,13)]
 species <- unique(Bees_all$FULL_SPECIES)
 
-all_recs_sp_only <- Bees_all$FULL_SPECIES
-
-#### Creating species frequency table
+############ Creating species frequency table #############
 sp_freq <- data.frame(Species=character(), Counts=integer())
 for(i in 1:length(species)){
-  newdata <- subset(Bees_from2003, FULL_SPECIES == species[i]) # Enter the YEAR_selection here
+  newdata <- subset(Bees_all, FULL_SPECIES == species[i]) 
   counts <- nrow(newdata)
   sp_row <- matrix(c(as.character(species[i]),counts), nrow=1)
   sp_freq <- rbind(sp_freq, sp_row)
 }
+rm(sp_row)
 colnames(sp_freq) <- c("Species", "Counts")
 sp_freq$Counts <- as.numeric(as.character(sp_freq$Counts))
-######
+############################################################
 
+# Subsetting the South part of the Netherlands
 
 # Creating selection of species with minumum record
 subset_100 <- subset(sp_freq, Counts > 100)
@@ -39,13 +36,16 @@ subset_counts_100 <- as.vector(sp_freq$Counts[sp_freq[2] > 100])
 nrow(subset_100)
 
 # Creating SUBSETTED TABLE   - - - !! CHECK year and species treshold in name !!
-Bees_subset_2003_100 <- subset(Bees_from2003, FULL_SPECIES == subset_sp_100[1])
+Bees_subset_2003_100 <- subset(Bees_all, FULL_SPECIES == subset_sp_100[1])
 for(i in 2:length(subset_sp_100)){
-  newdata <- subset(Bees_from2003, FULL_SPECIES == subset_sp_100[i]) # Enter the YEAR_selection here
+  newdata <- subset(Bees_all, FULL_SPECIES == subset_sp_100[i]) # Enter the YEAR_selection here
   Bees_subset_2003_100 <- rbind(Bees_subset_2003_100, newdata)
 }
-Bees_subset_2003_100 <- droplevels(Bees_subset_2003_100)
+rm(newdata)
 
+# Write to table
+Bees_subset_2003_100 <- droplevels(Bees_subset_2003_100)
+write.table(Bees_subset_2003_100, "data/Bee_data/Bee_data_SDM_input.csv")
 
 
 
