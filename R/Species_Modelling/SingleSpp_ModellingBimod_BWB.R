d054796617ac9bbb76a125e206572d939afca9c5
BasBoek <bastiaen.boekelo@wur.nl>
2015-07-14 17:05:42 +0200

    Writing gri and reading gri. Still need to write rasters at suitable location. Chosen to do ensemble modelling in order to get 1 evaluation score for every species based on all GLM's run. Need to implement that as well.

--- R/Species_Modelling/SingleSpp_ModellingBimod_BWB.R
+++ R/Species_Modelling/SingleSpp_ModellingBimod_BWB.R
@@ -25,7 +25,7 @@ predictor.files
 DataSpecies <- read.csv("data/Bee_data/Bee_data_SDM_input_All_sp_sep_columns.csv", sep="", header=T)
 
 # the name of studied species
-myRespName <- "Lasioglossum_sexstrigatum"
+myRespName <- "Nomada_alboguttata"
 
 # the presence/absences data for our species 
 myResp <- as.numeric(DataSpecies[,myRespName])
@@ -49,27 +49,15 @@ myBiomodData <- BIOMOD_FormatingData(resp.var = myResp,
                                      PA.nb.absences = 1250, # One species has 914 records. There are 2643 unique locations. Choosing 1000 PA is quite a lot (which is good for GLM SDM, and it can vary enough to see if there is many variation in model performance / var_imp etc..). 
                                      PA.strategy = 'random')
 
-# 
-# ###################################################
-# ### code chunk number 4: print_formating_data
-# ###################################################
-# myBiomodData
-# 
-# 
-# ###################################################
-# ### code chunk number 5: plot_formating_data
-# ###################################################
 # plot(myBiomodData)
-# 
-# 
+
 
 # ###################################################
 # ### code chunk number 7: MODELLING
 # ###################################################
-# # 3. Computing the models 
 
 myBiomodOption <- BIOMOD_ModelingOptions()
-setwd("D://SDM/SDM_Output")
+setwd("D:/SDM/SDM_Output")
 myBiomodModelOut <- BIOMOD_Modeling( 
                            myBiomodData, 
                            models = c('GLM'), 
@@ -81,7 +69,7 @@ myBiomodModelOut <- BIOMOD_Modeling(
                            models.eval.meth = c('ROC', 'TSS', 'ACCURACY'),
                            SaveObj = TRUE,
                            rescal.all.models = TRUE,
-                           do.full.models = FALSE,
+                           do.full.models = FALSE)
                            modeling.id = paste("Model_", myRespName, "_BWB", sep=""))
 
 #####################################################################################
@@ -104,18 +92,21 @@ Eval$Statistic <- substring(Rownames, 1, nchar(Rownames)-13)
 Eval$Run <- as.numeric(substring(Rownames, nchar(Rownames)-4, nchar(Rownames)-4))
 Eval$PA <- as.numeric(substring(Rownames, nchar(Rownames), nchar(Rownames)))
 
-write.table(Eval, paste("D:/SDM/SDM_Output/", myRespName, "_Evaluation.txt", sep=""),sep=" ")
-write.table(VarImp, paste("D:/SDM/SDM_Output/", myRespName, "_Variable_Imp.txt", sep=""),sep=" ")
+write.table(Eval, paste("D:/SDM/SDM_Output/_Evaluation/", myRespName, "_Evaluation.txt", sep=""),sep=" ", row.names=F)
+write.table(VarImp, paste("D:/SDM/SDM_Output/_Var_imp/", myRespName, "_Variable_Imp.txt", sep=""),sep=" ", row.names=F)
 
 ###################################################
 ### code chunk number 13: projection
 ###################################################
 # projection over the South NL under current conditions
 
+getwd()
+names(myExpl)
+names(myExpl_completearea)
 workspace_full_area <- paste("D:/SDM/Input_Rasters_KM2/full_area/", foldername_predictors, sep="")
 predictors_full <- list.files(workspace_full_area, pattern='.tif$', full.names=T)
 myExpl_completearea <- stack(predictors_full)
-
+str(myExpl_completearea)
 myBiomodProj <- BIOMOD_Projection(
                          modeling.output = myBiomodModelOut,
                          new.env = myExpl_completearea,
@@ -123,29 +114,23 @@ myBiomodProj <- BIOMOD_Projection(
                          selected.models = 'all',
                          binary.meth = c(NULL, 'ROC'),
                          compress = 'xz',
-                         clamping.mask = F,
-                         output.format = '.tif')
-
-# summary of created oject
-myBiomodProj
-
-# files created on hard drive
-list.files("Anthcard/proj_current/")
+                         build.clamping.mask = F,
+                         do.stack=T)
 
 
+# # summary of created oject
+# plot(myBiomodProj)
+# plot(myBiomodProj, str.grep = 'GLM')
+# 
+# # if you want to make custom plots, you can also get the projected map
+# myCurrentProj <- get_predictions(myBiomodProj)
 
-###################################################
-### code chunk number 14: projection_curent_plot
-###################################################
-# make some plots sub-selected by str.grep argument
-plot(myBiomodProj, str.grep = 'GLM')
+# Read the written raster files
+genus_name <- as.character(as.data.frame(strsplit(myRespName, "_"))[1,])
+species_name  <- as.character(as.data.frame(strsplit(myRespName, "_"))[2,])
 
+Pred_ROC_bin <- stack(paste("D:/SDM/SDM_Output/", genus_name, ".", species_name, "/proj_current/proj_current_", genus_name, ".", species_name, "_ROCbin.gri", sep=""))
 
-###################################################
-### code chunk number 15: projection_curent_getProj
-###################################################
-# if you want to make custom plots, you can also get the projected map
-myCurrentProj <- get_predictions(myBiomodProj)
-myCurrentProj
-
-
+Pred_Suit_scores <- stack(paste("D:/SDM/SDM_Output/", genus_name, ".", species_name, "/proj_current/proj_current_", genus_name, ".", species_name, sep=""))
+plot(Pred_Suit_scores)
+plot(Pred_ROC_bin)
